6a03fc864a1723cb794216410ac9689615d6bead
ManoSchyns <mano@schyns.net>
2026-04-21 07:59:45 +0200

    ajout du .sh pour run tout d'un  coup

---
+++ testing/run.sh
@@ -0,0 +1,84 @@
+!/bin/bash
+
+gcc -I. libft_test/testing/ft_atoi_test.c -L. -lft -o go
+./go 
+gcc -I. libft_test/testing/ft_bzero_test.c -L. -lft -o go
+./go 
+gcc -I. libft_test/testing/ft_isalnum_test.c -L. -lft -o go
+./go
+gcc -I. libft_test/testing/ft_isalpha_test.c -L. -lft -o go
+./go 
+gcc -I. libft_test/testing/ft_isascii_test.c -L. -lft -o go
+./go 
+gcc -I. libft_test/testing/ft_isdigit_test.c -L. -lft -o go
+./go 
+gcc -I. libft_test/testing/ft_isprint_test.c -L. -lft -o go
+./go 
+gcc -I. libft_test/testing/ft_itoa_test.c -L. -lft -o go
+./go 
+gcc -I. libft_test/testing/ft_lstadd_back_test.c -L. -lft -o go
+./go 
+gcc -I. libft_test/testing/ft_lstadd_front_test.c -L. -lft -o go
+./go 
+gcc -I. libft_test/testing/ft_lstclear_test.c -L. -lft -o go
+./go 
+gcc -I. libft_test/testing/ft_lstdelone_test.c -L. -lft -o go
+./go
+gcc -I. libft_test/testing/ft_lstiter_test.c -L. -lft -o go
+./go 
+gcc -I. libft_test/testing/ft_lstlast_test.c -L. -lft -o go
+./go 
+gcc -I. libft_test/testing/ft_lstmap_test.c -L. -lft -o go
+./go 
+gcc -I. libft_test/testing/ft_lstnew_test.c -L. -lft -o go
+./go
+gcc -I. libft_test/testing/ft_lstsize_test.c -L. -lft -o go
+./go 
+gcc -I. libft_test/testing/ft_memchr_test.c -L. -lft -o go
+./go 
+gcc -I. libft_test/testing/ft_memcpy_test.c -L. -lft -o go
+./go 
+gcc -I. libft_test/testing/ft_memmove_test.c -L. -lft -o go
+./go 
+gcc -I. libft_test/testing/ft_memset_test.c -L. -lft -o go
+./go 
+gcc -I. libft_test/testing/ft_putchar_fd_test.c -L. -lft -o go
+./go 
+gcc -I. libft_test/testing/ft_putendl_fd_test.c -L. -lft -o go
+./go
+gcc -I. libft_test/testing/ft_putnbr_fd_test.c -L. -lft -o go
+./go 
+gcc -I. libft_test/testing/ft_putstr_fd_test.c -L. -lft -o go
+./go 
+gcc -I. libft_test/testing/ft_split_test.c -L. -lft -o go
+./go 
+gcc -I. libft_test/testing/ft_strchr_test.c -L. -lft -o go
+./go 
+gcc -I. libft_test/testing/ft_strdup_test.c -L. -lft -o go
+./go 
+gcc -I. libft_test/testing/ft_striteri_test.c -L. -lft -o go
+./go 
+gcc -I. libft_test/testing/ft_strjoin_test.c -L. -lft -o go
+./go 
+gcc -I. libft_test/testing/ft_strlcat_test.c -L. -lft -lbsd -o go
+./go
+gcc -I. libft_test/testing/ft_strlcpy_test.c -L. -lft -lbsd -o go
+./go 
+gcc -I. libft_test/testing/ft_strlen_test.c -L. -lft -lbsd -o go
+./go 
+gcc -I. libft_test/testing/ft_strmapi_test.c -L. -lft -lbsd -o go
+./go
+gcc -I. libft_test/testing/ft_strncmp_test.c -L. -lft -lbsd -o go
+./go 
+gcc -I. libft_test/testing/ft_strnstr_test.c -L. -lft -lbsd -o go
+./go
+gcc -I. libft_test/testing/ft_strrchr_test.c -L. -lft -lbsd -o go
+./go
+gcc -I. libft_test/testing/ft_strtrim_test.c -L. -lft -lbsd -o go
+./go
+gcc -I. libft_test/testing/ft_substr_test.c -L. -lft -lbsd -o go
+./go 
+gcc -I. libft_test/testing/ft_tolower_test.c -L. -lft -lbsd -o go
+./go 
+gcc -I. libft_test/testing/ft_toupper_test.c -L. -lft -lbsd -o go
+./go
